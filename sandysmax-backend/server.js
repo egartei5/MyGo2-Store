c899513d2cf6095541ad9d6f774bfe759202bea5
Enoch Gartei <enochgartei@Enochs-MacBook-Pro.local>
2025-05-15 13:12:17 -0500

    Fixed backend, updated checkout page, ready to deploy live

---
+++ sandysmax-backend/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/create-payment-intent', async (req, res) => {
+  const { amount } = req.body;
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: 'usd',
+      payment_method_types: ['card'],
+    });
+
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error) {
+    res.status(400).send({
+      error: {
+        message: error.message,
+      },
+    });
+  }
+});
+
+const PORT = process.env.PORT || 4242;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
